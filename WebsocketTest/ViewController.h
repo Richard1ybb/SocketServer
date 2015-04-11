e546409e2886639d82a326253864c669e40be077
foundry <foundry.tv@gmail.com>
2015-04-11 19:25:38 +0100

    Initial Commit

---
+++ WebsocketTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebsocketTest
+//
+//  Created by jonathan on 11/04/2015.
+//  Copyright (c) 2015 net.ellipsis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ WebsocketTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  WebsocketTest
+//
+//  Created by jonathan on 11/04/2015.
+//  Copyright (c) 2015 net.ellipsis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
