95e753e969dcfcc87d9e5f07a28f2e814c4bddda
foundry <foundry.tv@gmail.com>
2015-04-13 10:54:11 +0100

    added bonjour publishing

--- SocketServer/ViewController.m
+++ SocketServer/ViewController.m
@@ -10,15 +10,19 @@
 #import "MBWebSocketServer.h"
 #import "GCDAsyncSocket.h"
 
-@interface ViewController()<MBWebSocketServerDelegate>
+@interface ViewController()<MBWebSocketServerDelegate, NSNetServiceDelegate>
 @property (nonatomic, strong) MBWebSocketServer* ws;
+@property (nonatomic, strong) NSNetService* netService;
+@property (nonatomic, strong) NSString* serviceName;
 @end
 @implementation ViewController
 
 - (void)viewDidLoad {
     NSLog(@"%s",__func__);
-
+    uint16_t port = 9000;
     self.ws = [[MBWebSocketServer alloc] initWithPort:9000 delegate:self];
+    self.serviceName = @"socketsensors";
+    [self publishService:port];
     [super viewDidLoad];
 
     // Do any additional setup after loading the view.
@@ -53,4 +57,56 @@
     NSLog(@"MBWebSocketServer error: %@", error);
 }
 
+- (void)publishService:(uint16_t)chosenPort {
+    NSLog(@"chosenPort = %d", chosenPort);
+    
+    self.netService = [[NSNetService alloc] initWithDomain:@""
+                                                      type:@"_fnwebsockets._tcp."
+                                                      name:self.serviceName
+                                                      port:chosenPort];
+    if (self.netService != nil) {
+        [self.netService setDelegate:self];
+        [self.netService publishWithOptions:0];
+    }
+
+}
+
+
+#pragma mark - netserivce
+
+- (void)netServiceDidPublish:(NSNetService *)sender {
+    NSLog(@"%s, %@",__func__,sender);
+}
+
+- (void)netServiceDidResolveAddress:(NSNetService *)sender {
+    NSLog(@"%s, %@",__func__,sender);
+}
+
+- (void)netServiceDidStop:(NSNetService *)sender {
+    NSLog(@"%s, %@",__func__,sender);
+}
+
+- (void)netServiceWillPublish:(NSNetService *)sender {
+    NSLog(@"%s, %@",__func__,sender);
+}
+
+- (void)netServiceWillResolve:(NSNetService *)sender {
+    NSLog(@"%s, %@",__func__,sender);
+}
+
+- (void)netService:(NSNetService *)sender didNotPublish:(NSDictionary *)errorDict {
+    NSLog(@"%s, %@, %@",__func__,sender,errorDict);
+}
+
+- (void)netService:(NSNetService *)sender didAcceptConnectionWithInputStream:(NSInputStream *)inputStream outputStream:(NSOutputStream *)outputStream {
+    NSLog(@"%s, %@, %@",__func__,sender,outputStream);
+}
+
+- (void)netService:(NSNetService *)sender didNotResolve:(NSDictionary *)errorDict {
+    NSLog(@"%s, %@, %@",__func__,sender,errorDict);
+}
+
+- (void)netService:(NSNetService *)sender didUpdateTXTRecordData:(NSData *)data {
+    NSLog(@"%s, %@",__func__,sender);
+}
 @end
